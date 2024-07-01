ce21ca6473160d706cc3ae4704fe67ff4d4c1107
giuseppemaisto <gmaisto98@gmail.com>
2024-07-01 22:58:48 +0200

    primo commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Backend di Conart</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color:black
+               
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 48px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <div class="title">
+                backend di Conart
+            </div>
+        </div>
+    </body>
+</html>
